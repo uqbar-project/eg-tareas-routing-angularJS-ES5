2e33574ed2276812940aa3781e16e78f23d88f3b
Martin Dias <tinchodias@gmail.com>
2017-10-16 22:04:25 -0300

    Minimo refactoring en routes y partials
    Apunto a quitar cosas que distraigan del punto central de este ejemplo

--- js/routes.js
+++ js/routes.js
@@ -1,19 +1,15 @@
 tareasListApp.config(function ($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise("/");
-
   $stateProvider
-
     .state('agregarTarea', {
       url: "/",
-      templateUrl: "partials/agregar_tarea.html",
-      controller: "AgregarTareaController as tareasCtrl"
+      templateUrl: "partials/agregar_tarea.html"
     })
-
     .state('editarTarea', {
       url: "/tarea/:id",
-      templateUrl: "partials/editar_tarea.html",
-      controller: "EditarTareaController as editarCtrl"
-    })
+      templateUrl: "partials/editar_tarea.html"
+    });
+
+    $urlRouterProvider.otherwise("/");
 
 });
